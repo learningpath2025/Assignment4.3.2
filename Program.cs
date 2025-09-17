0334740df1324b21d62e69ce4a07b53e205a569a
learningpath2025 <learningpath2025@outlook.com>
2025-09-16 20:35:03 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿namespace Assignment4._3._2
+{
+    public class Program
+    {
+        static List<int> list = new List<int>();
+        static void Main(string[] args)
+        {
+            GetInputList();
+            ElementFrequencyCounter();
+        }
+
+        public static void GetInputList()
+        {
+            Console.Write($"Input the number of elements to be stored in the array: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Input {n} elements in the array: ");
+            
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Element - {i + 1}: ");
+                list.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+        }
+
+        public static void ElementFrequencyCounter()
+        {
+            var frequency = list.GroupBy(x => x)
+                             .ToDictionary(x => x.Key, x => x.Count());
+
+            foreach (var element in frequency)
+            {
+                Console.WriteLine($"{element.Key} appears {element.Value} times.");
+            }
+        }
+
+
+        //Alternative Method
+        /*
+        public static void ElementFrequencyCounter()
+        {
+            Dictionary<int, int> frequency = new Dictionary<int, int>();
+
+            foreach (int x in list)
+            {
+                if (frequency.ContainsKey(x))
+                {
+                    frequency[x]++;
+                }
+                else
+                {
+                    frequency[x] = 1;
+                }
+            }
+
+            foreach (var element in frequency)
+            {
+                Console.WriteLine($"{element.Key} appears {element.Value} times.");
+            }
+        }
+        */
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Assignment4.3.2
+
+- Write a program in C# Sharp to count the frequency of each element of an array.
+- Test Data :
+	- Input the number of elements to be stored in the array :3
+	- Input 3 elements in the array :
+		- element - 0 : 25
+		- element - 1 : 12
+		- element - 2 : 43
+	- Expected Output :
+	- Frequency of all elements of array :
+		- 25 occurs 1 times
+		- 12 occurs 1 times
+		- 43 occurs 1 times
+- Hints: 
+	- Get input elements in a list.
+	- Use grouping and dictionary.
